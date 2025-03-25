81800c5efab8c914339c649abf67d42ca9c72e6f
JoseAyeras <jose_ayeras@yahoo.com>
2025-03-25 07:16:58 -0700

    Update proxy.c
    created opt codes

--- Code/Proxy/proxy.c
+++ Code/Proxy/proxy.c
@@ -1,8 +1,41 @@
+
+#include <argp.h>	//needed for arg parsing
+#include <stdio.h>
+#include <stdlib.h>	//needed for float params
+
+
 // Keys for options without short options.
-#define OPT_PROXY_IP 1
-#define OPT_SERVER_IP 2
-#define OPT_PORT 3
-#define OPT_
+// We may not actually need it but the documentation here is useful regardless.
+enum opt_codes {
+    //there are 18 optcodes, the bits are as follows:
+    //^4  0| proxy behavior     1| ip address
+    //^3  01| broad     00| local
+    //^2  11| broad delay++   10| both client and server      01| client      00| server
+    //^1  111| broad delay m* 110| broad delay/port     1| delay time    0| chances
+    //^0  11| max delay 10| min delay   01| delay chance        00| drop chance
+     OPT_SDRP	//0     server drop chance
+    ,OPT_SDLY	//1     server delay chance
+    ,OPT_STMN	//2     server delay time minimum
+    ,OPT_STMX	//3     server delay time maximum
+    
+    ,OPT_CDRP	//4     client drop chance
+    ,OPT_CDLY	//5     client delay chance
+    ,OPT_CTMN	//6     client delay time minimum
+    ,OPT_CTMX   //7     client delay time maximum
+    
+    ,OPT_UDRP	//8     drop chance
+    ,OPT_UDLY	//9     delay chance
+    ,OPT_UTMN	//A     delay time minimum
+    ,OPT_UTMX   //B     delay time maximum
+    
+    ,OPT_CDRP	//C     server delay time
+    ,OPT_CDLY	//D     client delay time
+    ,OPT_CTMN	//E     delay time
+    ,OPT_CTMX   //F     port
+    
+    ,OPT_CDRP	//10    server ip address
+    ,OPT_CDLY	//11    proxy ip address
+}
 
 /* Used by main to communicate with parse_opt. */
 struct arguments
@@ -58,9 +91,100 @@ parse_opt (int key, char *arg, struct argp_state *state)
     struct arguments *arguments = state->input;
         switch (key)
     {
-
+	case 0x00:
+            //server drop chance
+            arguments->server_drop_chance = strtof(arg, 0);
+        break;
+	case 0x01:
+	    //server delay chance
+            arguments->server_delay_chance = strtof(arg, 0);
+        break;
+	case 0x02:
+	    //server delay time minimum
+            arguments->server_delay_time_min = strtof(arg, 0);
+        break;
+	case 0x03:
+	    //server delay time maximum
+            arguments->server_delay_time_max = strtof(arg, 0);
+        break;
+        
+	case 0x04:
+	    //client drop chance
+            arguments->client_drop_chance = strtof(arg, 0);
+        break;
+	case 0x05:
+	    //client delay chance
+            arguments->client_delay_chance = strtof(arg, 0);
+        break;
+	case 0x06:
+	    //client delay time minimum
+            arguments->client_delay_time_min = strtof(arg, 0);
+        break;
+	case 0x07:
+	    //client delay time maximum
+            arguments->client_delay_time_max = strtof(arg, 0);
+        break;
+        
+	case 0x08:
+	    //drop chance
+            arguments->server_drop_chance = strtof(arg, 0);
+            arguments->client_drop_chance = strtof(arg, 0);
+        break;
+	case 0x09:
+	    //delay chance
+            arguments->server_delay_chance = strtof(arg, 0);
+            arguments->client_delay_chance = strtof(arg, 0);
+        break;
+	case 0x0A:
+	    //delay time minimum
+            arguments->server_delay_time_min = strtof(arg, 0);
+            arguments->client_delay_time_min = strtof(arg, 0);
+        break;
+	case 0x0B:
+	    //delay time maximum
+            arguments->server_delay_time_max = strtof(arg, 0);
+            arguments->client_delay_time_max = strtof(arg, 0);
+        break;
+        
+	case 0x0C:
+	    //server delay time
+            arguments->server_delay_time_min = strtof(arg, 0);
+            arguments->server_delay_time_max = strtof(arg, 0);
+        break;
+	case 0x0D:
+	    //client delay time
+            arguments->client_delay_time_min = strtof(arg, 0);
+            arguments->client_delay_time_max = strtof(arg, 0);
+        break;
+	case 0x0E:
+	    //delay time
+            arguments->server_delay_time_min = strtof(arg, 0);
+            arguments->server_delay_time_max = strtof(arg, 0);
+            arguments->client_delay_time_min = strtof(arg, 0);
+            arguments->client_delay_time_max = strtof(arg, 0);
+        break;
+	case 0x0F:
+	    //port
+	    arguments->port = atoi(arg);
+        break;
+        
+	case 0x10:
+	    //server ip address
+	    arguments->server_ip = arg;
+        break;
+	case 0x11:
+	    //proxy ip address
+	    arguments->proxy_ip = arg;
         break;
     default:
       return ARGP_ERR_UNKNOWN;
     }
+}
+
+
+
+int handle_args(int argc, char** argv){
+    struct argp parser = { options, parse_opt, args_doc, doc };
+    struct arguments arguments;
+    
 }
